5ba06f1fcd20074764a1e8439bb7ce09e4d9dd5d
Simon <simon.k.lonnestad@gmail.com>
2020-04-27 19:21:23 +0200

    Check if difficulty & README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Clue-Scroll-API
+A simple Clue Scroll API for Old School RuneScape
+
+## How to run
+Place `index.php` in your webserver
+
+Enter your webserver with following parameters:
+`http://<webserver>/?difficulty=master`
+
+The difficulty of the clue scroll reward is defined in the `difficulty` parameter
+
+Only `beginner`, `easy`, `medium`, `hard`, `elite` and `master` are accepted
+
+## Expected results:
+*Master tier rewards:*
+
+![Master tier rewards](https://i.imgur.com/7YcyQIO.png)
--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
-	header('Content-type: application/json');
+	if ($_GET["difficulty"]) {
+		header('Content-type: application/json');
 
-	$getClueScroll = file_get_contents('https://oldschool.runescape.wiki/api.php?action=browsebysubject&format=json&subject=Reward_casket_('.$_GET["difficulty"].')');
+		$getClueScroll = file_get_contents('https://oldschool.runescape.wiki/api.php?action=browsebysubject&format=json&subject=Reward_casket_('.$_GET["difficulty"].')');
 
-	$clueScroll = json_decode($getClueScroll);
+		$clueScroll = json_decode($getClueScroll);
 
-	$rewards = $clueScroll->query->sobj;
+		$rewards = $clueScroll->query->sobj;
 
-	foreach ($rewards as $reward) {
-		$items[] = array(
-			/* $reward->data[3]->property) */ 'item' => $reward->data[3]->dataitem[0]->item,
-			/* $reward->data[6]->property) */ 'quantity-low' => $reward->data[6]->dataitem[0]->item,
-			/* $reward->data[5]->property) */ 'quantity-high' => $reward->data[5]->dataitem[0]->item,
-			/* $reward->data[7]->property) */ 'rarity' => $reward->data[7]->dataitem[0]->item,
-			/* $reward->data[9]->property) */ 'rolls' => $reward->data[9]->dataitem[0]->item,
-		);
+		foreach ($rewards as $reward) {
+			$items[] = array(
+				/* $reward->data[3]->property) */ 'item' => $reward->data[3]->dataitem[0]->item,
+				/* $reward->data[6]->property) */ 'quantity-low' => $reward->data[6]->dataitem[0]->item,
+				/* $reward->data[5]->property) */ 'quantity-high' => $reward->data[5]->dataitem[0]->item,
+				/* $reward->data[7]->property) */ 'rarity' => $reward->data[7]->dataitem[0]->item,
+				/* $reward->data[9]->property) */ 'rolls' => $reward->data[9]->dataitem[0]->item,
+			);
+		}
+
+		echo json_encode($items);
+	} else {
+		echo "missing difficulty. pick between beginner, easy, medium, hard, elite or master!";
 	}
-
-	echo json_encode($items);
 ?>
